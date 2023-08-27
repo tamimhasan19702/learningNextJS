f7376007e9a47ce51ae75e00d81f9ef0f308e591
tamimhasan19702 <tamimhasancu@gmail.com>
2023-08-28 02:03:47 +0600

    update page.js and page.module.css

--- src/app/page.js
+++ src/app/page.js
@@ -34,20 +34,24 @@ export default function Home() {
         A blog for hunting Coders by hunting coder tamim
       </p>
 
+    <div className="blogSection">
+      <h2 className={styles.blogIntro}>Popular Blogs</h2>
      <div className={styles.blogs}>
-      <div className={styles.blogItem}>
-        <h1>How to Learn javascript in 2022</h1>
-        <p>Javascript is one of the most popular programming languages in the world</p>
-      </div>
-      <div className={styles.blogItem}>
-        <h1>How to Learn javascript in 2022</h1>
-        <p>Javascript is one of the most popular programming languages in the world</p>
-      </div>
-      <div className={styles.blogItem}>
-        <h1>How to Learn javascript in 2022</h1>
-        <p>Javascript is one of the most popular programming languages in the world</p>
-      </div>
+       <div className={styles.blogItem}>
+         <h3>How to Learn javascript in 2022</h3>
+         <p>Javascript is one of the most popular programming languages in the world</p>
+       </div>
+       <div className={styles.blogItem}>
+         <h3>How to Learn javascript in 2022</h3>
+         <p>Javascript is one of the most popular programming languages in the world</p>
+       </div>
+       <div className={styles.blogItem}>
+         <h3>How to Learn javascript in 2022</h3>
+         <p>Javascript is one of the most popular programming languages in the world</p>
+       </div>
      </div>
+     
+    </div>
 
     </main>
     </>
