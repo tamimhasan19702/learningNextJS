f2d18d5630ea34733be298b404ef9d3b15726ac3
tamimhasan19702 <tamimhasancu@gmail.com>
2023-08-29 01:22:36 +0600

    update page.js and page.module.css

---
+++ src/app/blogpost/[slug]/page.js
@@ -0,0 +1,15 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import React from 'react'
+import { useRouter } from 'next/router'
+
+const slug = () => {
+
+  const router = useRouter();
+  const { slug } = router.query
+
+  return (
+    <div>{slug}</div>
+  )
+}
+
+export default slug
