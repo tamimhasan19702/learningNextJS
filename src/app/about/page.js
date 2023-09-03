440340766a249c4449e508491e4632d96dd0fcc8
tamimhasan19702 <tamimhasancu@gmail.com>
2023-09-04 00:03:49 +0600

    update page.js, page.js and page.js

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 const about = () => {
   return (
-    <div>about</div>
+    <div>
+    <h1>About Me</h1>
+    <p>
+      I am a blogger who is also a coder. I love sharing my experiences and knowledge through my blog posts.
+    </p>
+    <p>
+      In this blog, You will find articles about coding, programming languages, web development, and much more.
+    </p>
+  </div>
   )
 }
 
--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,9 +1,60 @@
-import React from 'react'
+/** @format */
+
+import React from "react";
+import styles from "../styles/Blog.module.css";
+import Link from "next/link";
 
 const Blog = () => {
   return (
-    <div>Blog</div>
-  )
-}
+    <div className={styles.main}>
+      <div className="blogSection">
+        <h2 className={styles.blogIntro}>Popular Blogs</h2>
 
-export default Blog
+        <div className={styles.blogs}>
+          
+          <div className={styles.blogItem}>
+            <Link href={"/blogpost/learn-javascript"}>
+              <h3 className={styles.blogHead}>
+                How to Learn javascript in 2022
+              </h3>
+
+              <p>
+                Javascript is one of the most popular programming languages in
+                the world
+              </p>
+            </Link>
+          </div>
+
+          <div className={styles.blogItem}>
+            <Link href={"/blogpost/learn-javascript"}>
+              <h3 className={styles.blogHead}>
+                How to Learn javascript in 2022
+              </h3>
+
+              <p>
+                Javascript is one of the most popular programming languages in
+                the world
+              </p>
+            </Link>
+          </div>
+
+          <div className={styles.blogItem}>
+            <Link href={"/blogpost/learn-javascript"}>
+              <h3 className={styles.blogHead}>
+                How to Learn javascript in 2022
+              </h3>
+
+              <p>
+                Javascript is one of the most popular programming languages in
+                the world
+              </p>
+            </Link>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
