e2fa109a88f846de3869b08ef22a0dd02a3b4c16
tamimhasan19702 <tamimhasancu@gmail.com>
2023-09-11 21:33:00 +0600

    update route.js

--- src/app/api/hello/route.js
+++ src/app/api/hello/route.js
@@ -1,12 +1,19 @@
-export async function GET(request) {
-    const data = {
-        message: "hello tamim hasan"
-    };
+/** @format */
 
-    return new Response(JSON.stringify(data), {
-        status: 200,
-        headers: {
-            "Content-Type": "application/json"
-        }
-    });
+import * as fs from "fs";
+
+
+export async function GET(request) {
+
+  data1 = {"message": "Hello "}
+
+  // fs.readFile('blogdata/how-to-learn-javascript.json', 'utf-8',(data) => {
+  //  console.log(data)
+  // });
+
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+
+  return new Response(data1, { status: 200, headers });
 }
